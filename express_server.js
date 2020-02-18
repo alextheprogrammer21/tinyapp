a1459f4992239b5480200a0f500e0d236ee959bb
Alex <alextheprogrammer21@gmail.com>
2020-02-18 03:24:12 +0000

    Added a random string generator

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -16,6 +18,10 @@ app.get('/', (req, res) => {
 app.get('/urls.json', (req,res) => {
   res.json(urlDatabase);
 })
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -41,8 +47,22 @@ app.get('/fetch', (req, res) => {
   res.send(`a = ${a}`);
 });
 
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 app.listen(PORT, () => {
 console.log("Listening on port", PORT);
 });
+
+function generateRandomString() {
+  let randomString = '';
+  randomString += Math.round(Math.random()*10);
+  randomString += Math.round(Math.random()*10);
+  randomString += Math.round(Math.random()*10);
+  randomString += Math.round(Math.random()*10);
+  randomString += Math.round(Math.random()*10);
+  randomString += Math.round(Math.random()*10);
+}
 
 module.exports = {urlDatabase};
