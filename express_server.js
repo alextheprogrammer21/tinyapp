170c400939b663ca7eeafb825019cce41a46a395
Alex <alextheprogrammer21@gmail.com>
2020-02-18 01:57:32 +0000

    Added ejs templates and partials

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,13 +17,24 @@ app.get('/urls.json', (req,res) => {
   res.json(urlDatabase);
 })
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get('/hello', (req,res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
 
 app.get('/set', (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
 });
 
 app.get('/fetch', (req, res) => {
@@ -31,3 +44,5 @@ app.get('/fetch', (req, res) => {
 app.listen(PORT, () => {
 console.log("Listening on port", PORT);
 });
+
+module.exports = {urlDatabase};
